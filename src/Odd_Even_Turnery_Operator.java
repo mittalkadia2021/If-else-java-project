2840abb157f471c70e847c6f018a9b5de645b86b
Mittal <mittalkadia2021@gamil.com>
2021-03-23 22:53:12 +0000

    odd even class

--- src/Odd_Even_Turnery_Operator.java
+++ src/Odd_Even_Turnery_Operator.java
@@ -2,11 +2,11 @@ public class Odd_Even_Turnery_Operator
 {
     public static void Modular() {
         int Number = 20;
-        int number1 = 35;
+        int number1 = 41;
         //Find out Odd or even number use turnery operator
-        String result = (Number % 2 == 0) ? "Number 20 is odd" : "Number 20 is even";
+        String result = (Number % 2 == 0) ? "Number "+ Number + " is even" : "Number"+  Number + " is odd";
         System.out.println(result);
-        String result1 = (number1 % 2 == 0) ? "Number 35 is odd" : "Number 35 is even";
+        String result1 = (number1 % 2 == 0) ? "Number"+ number1 + " is even" : "Number "  + number1 + " is odd";
         System.out.println(result1);
     }
     public static void main(String[] args)
